6b15745c3ada2978243c38b45f806d5c80faa670
Voidable <randomkingwholikespink@gmail.com>
2015-10-06 13:37:42 -0400

    Commit local files

---
+++ RetroFalcons_TicTacToeGame-master/RetroFalcons_TicTacToeGame/RetroFalcons_TicTacToeGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroFalcons_TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create game controller object
+            GameController controller = new GameController();
+
+            //  Instruct controller to run the game
+            controller.RunGameLogic();
+        }
+    }
+}
